6d9c0b52f7b98cd7d91e27ef575ac39ecbca5870
SHIN_NARITA <triplfjump1417@gmail.com>
2016-12-22 20:29:54 +0900

    bolt__conplete

---
+++ js/script.js
@@ -0,0 +1,5 @@
+(function() {
+  document.querySelector(".js-menu-bar").addEventListener("click", function() {
+    document.querySelector(".js-menu-btn").classList.toggle("is-active");
+  }, false);
+})();
