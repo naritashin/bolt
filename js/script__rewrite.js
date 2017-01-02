6377c4c90ea0379073d71db765171df5c031a2a7
SHIN_NARITA <triplfjump1417@gmail.com>
2017-01-03 08:07:50 +0900

    書き直し2017_1_3

---
+++ js/script__rewrite.js
@@ -0,0 +1,5 @@
+(function() {
+  document.querySelector(".header__menu__bars").addEventListener("click", function() {
+    document.querySelector(".header__menu__nav").classList.toggle("is-active");
+  }, false);
+})();
